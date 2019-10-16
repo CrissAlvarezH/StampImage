cc1331016fb71a81747e93bdd8813ba91b916fa7
Cristian Alvarez H <alvarezhernandezcristian1@gmail.com>
2019-10-16 15:59:03 -0500

    Set textSize of the 6% of the witdh of image

--- app/src/main/java/com/cristian/dev/stampimage/utils/ImageUtils.kt
+++ app/src/main/java/com/cristian/dev/stampimage/utils/ImageUtils.kt
@@ -5,26 +5,38 @@ import java.io.FileNotFoundException
 import java.io.FileOutputStream
 import android.graphics.BitmapFactory
 import android.graphics.Bitmap
+import android.media.ExifInterface
 import android.os.Environment
+import android.util.Log
 
+private const val TAG = "UtilsImagen";
 
 class ImageUtils {
 
     companion object {
 
         fun stampImage(textStamp: String, rutaImg: String, nombreImgDestino: String): String? {
+
+            Log.v(TAG, "Orientación: ${ getOrientacion(rutaImg) }")
+
             // Creamos un bitmap de la imagen con la ruta pasada por parametros
             val options = BitmapFactory.Options()
             options.inPreferredConfig = Bitmap.Config.ARGB_8888
             val bitmap = BitmapFactory.decodeFile(rutaImg, options)
 
+            Log.v(TAG, "Original. Width: ${ bitmap.width }, Height: ${ bitmap.height }")
+
             // Creamos otro bitmap que contendrá la imagen ya estampada
             val bitmapDestino = Bitmap.createBitmap(bitmap.width, bitmap.height, Bitmap.Config.ARGB_8888)
+
+            Log.v(TAG, "Destino. Width: ${ bitmap.width }, Height: ${ bitmap.height }")
 
             val canvas = Canvas(bitmapDestino) // Cambas con le bitmap creado para detino
 
             // Creamos el pincel con el que pintaremos el texto en la imagen
-            val textSize = 200f
+            val textSize = bitmap.width * 0.06f; // Porcentaje del ancho de la imagen
+
+            Log.v(TAG, "TextSize: ${ textSize }")
 
             val paintText = Paint()
             paintText.style = Paint.Style.FILL
@@ -71,6 +83,22 @@ class ImageUtils {
             }
 
             return rutaDestino;
+        }
+
+        fun getOrientacion(rutaImg: String): Int {
+
+            val imgFile = File(rutaImg)
+
+            val exif = ExifInterface(imgFile.absolutePath)
+
+            /* Grados de la clase ExifInterface
+            Normal = 1
+            Indefinido = 0
+            180 = 3
+            270 = 8
+            90 = 6
+             */
+            return exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL)
         }
 
     }
